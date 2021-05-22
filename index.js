19a4c09bc04882b9482eb7f3f92b80487092a131
slackATZ <atuzirou.overjoyed@gmail.com>
2021-05-22 18:39:07 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+app.event('app_home_opened', ({ event, say }) => {
+    say(`Hello world, <@${event.user}>!`);
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+})();
